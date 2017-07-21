4017dcea42980f722df36a8c0cfa1d1ed1d42457
Zavin Hussein <zavinhussein@gmail.com>
2017-07-22 01:39:29 +0300

    Finished the App

--- app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
+++ app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
@@ -2,14 +2,63 @@ package com.hussein.zavin.thebuttoncounterapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTEXT = "TextContexts";
+    private EditText userInput;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        userInput.setText("");
+
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String result = userInput.getText().toString();
+                result += "\n";
+                textView.append(result);
+                userInput.setText("");
+            }
+        };
+        button.setOnClickListener(ourOnClickListener);
+
+        Log.d(TAG, "onCreate: out");
+
     }
 
-    //More Stuff to go here like dunno, button click listener
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(TEXT_CONTEXT));
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(TEXT_CONTEXT, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: out");
+    }
+
 }
