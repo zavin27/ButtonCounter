9bbc67cb609844e5b30002187ab39a79bcc04420
Zavin Hussein <zavinhussein@gmail.com>
2017-07-21 20:03:53 +0300

    Test

--- app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
+++ app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
@@ -1,7 +1,7 @@
 package com.hussein.zavin.thebuttoncounterapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //
+    //More Stuff to go here like dunno, button click listener
 }
