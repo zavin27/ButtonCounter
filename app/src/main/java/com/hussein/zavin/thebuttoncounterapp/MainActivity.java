a2aa41c370687e3f7e93c83497efb64f48605d1a
Zavin Hussein <zavinhussein@gmail.com>
2017-07-22 01:52:06 +0300

    Code clean up

--- app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
+++ app/src/main/java/com/hussein/zavin/thebuttoncounterapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.hussein.zavin.thebuttoncounterapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,14 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
     private final String TEXT_CONTEXT = "TextContexts";
     private EditText userInput;
     private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -40,25 +37,20 @@ public class MainActivity extends AppCompatActivity {
         };
         button.setOnClickListener(ourOnClickListener);
 
-        Log.d(TAG, "onCreate: out");
 
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.d(TAG, "onRestoreInstanceState: in");
         super.onRestoreInstanceState(savedInstanceState);
         textView.setText(savedInstanceState.getString(TEXT_CONTEXT));
-        Log.d(TAG, "onRestoreInstanceState: out");
     }
 
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        Log.d(TAG, "onSaveInstanceState: in");
         outState.putString(TEXT_CONTEXT, textView.getText().toString());
         super.onSaveInstanceState(outState);
-        Log.d(TAG, "onSaveInstanceState: out");
     }
 
 }
